9cb6ad9c61734c31c3bd46e96e67c7aeae1cdd57
Valen DeLeon <valeriedeleon4521@gmail.com>
2025-05-29 22:11:06 -0700

    Delete generateSVG.py

--- generateSVG.py
+++
@@ -1,8 +0,0 @@
-from pathLib import Path
-
-template = Path("assets/hello.svg").read_text()
-
-for theme in ["light", "dark"]:
-  css = Path(f"assets/{theme}-hello.css").read_text()
-  output = template.replace("/* THEME_CSS */", css)
-  Path(f"output/{theme}.svg").write_text(ouput)
